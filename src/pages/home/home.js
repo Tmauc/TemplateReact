9f6aaf75f8860f91e49465fd90ffe396c180da49
Mauc <tmauccc@gmail.com>
2022-12-21 15:18:08 +0100

    Initial commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.scss'
+
+function Home() {
+  return (
+    <section>
+      <p>
+        Template React
+      </p>
+    </section>
+  )
+}
+
+export default Home
